006d9f4041dc8e60e08393771450842b0baa5d51
Anh Tan Pham <phamanhtan@proton.me>
2024-04-06 10:15:09 +0700

    remove prisma

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ProductService } from './product.service'
-import { PrismaModule } from '../common/connections/prisma.module'
 import { HttpModule } from '@nestjs/axios'
 import { HttpclientModule } from '../shares/httpclient/httpclient.module'
 import { ProductController } from './product.controller'
 
 @Module({
     imports: [
-        PrismaModule,
         HttpModule.registerAsync({
             useFactory: () => ({
                 timeout: 60000,
